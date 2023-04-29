080d6a83a03f7f5042f6225d5c6ba78751858218
Miles <milesrobertson222@gmail.com>
2023-04-29 06:04:44 -0400

    resolve setup errors

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import requests
+import pip._vendor.requests as requests
+
+# Making a GET request
+r = requests.get('https://api.github.com/users/naveenkrnl')
+if r.status_code != 200:
+    print("Error: Unsuccessful request")
+
+
+# check status code for response received
+# success code - 200
+print(r)
+
+# print content of request
+print(r.content)
+print("\nDone\n")
