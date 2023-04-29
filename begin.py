d31036a6f93bc9dd2f87b6922700099ede35c812
Miles <milesrobertson222@gmail.com>
2023-04-29 06:44:04 -0400

    compartmentalize approach1

--- begin.py
+++ begin.py
@@ -1,9 +1,9 @@
 import pip._vendor.requests as requests
 
 # Making a GET request
-r = requests.get('https://api.github.com/users/naveenkrnl')
+r = requests.get('https://www.googleapis.com/calendar/v3/users/me/calendarList')
 if r.status_code != 200:
-    print("Error: Unsuccessful request")
+    print("Error: Unsuccessful request\n")
 
 
 # check status code for response received
