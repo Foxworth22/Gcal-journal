97cabf6790b978937324b2d2c4d0d11f3472afa9
Miles <milesrobertson222@gmail.com>
2023-05-31 18:07:53 -0600

    create all_calendars variable

--- quickstart.py
+++ quickstart.py
@@ -93,7 +93,8 @@ def main():
         page_token = None
         while True:
             calendar_list = service.calendarList().list(pageToken=page_token).execute()
-            for calendar_list_entry in calendar_list['items']:
+            all_calendars = calendar_list['items']  # self-defined modulation for future re-use
+            for calendar_list_entry in all_calendars:
                 print (calendar_list_entry['summary'])
             page_token = calendar_list.get('nextPageToken')
             if not page_token:
